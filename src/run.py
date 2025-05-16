e6f4e96f8512694992ec77e60ac22fbe4fc0c9b4
Barrett Kepler <45532267+Kepler-Br@users.noreply.github.com>
2025-05-16 23:26:58 +0300

    Kafka producer test

--- src/run.py
+++ src/run.py
@@ -1,31 +1,65 @@
+import dataclasses
+import json
 import os
 import sys
 from io import BytesIO
 
 import uvicorn
+from confluent_kafka import Producer
 from minio import Minio
+
+from new_message_request import NewMessageRequest, NewMessageUser, NewMessageChat, ChatType, MessageType
 
 
 def main():
-    access_key = os.environ.get('MINIO_ACCESS_KEY')
-    secret_key = os.environ.get('MINIO_SECRET_KEY')
-    client = Minio("localhost:8000",
-                   secure=False,
-                   access_key=access_key,
-                   secret_key=secret_key,
-                   )
-    print(
-        client.bucket_exists("audio1")
-    )
-    with open("/home/kepler-br/Music/Ib/title.mp3", "rb") as fp:
-        read = fp.read()
+    config = {
+        'bootstrap.servers': 'localhost:9092',
+    }
 
-        client.put_object("audio", "audio_test.mp3", BytesIO(read), len(read))
+    producer = Producer(config)
+
+    def send_message(topic, message):
+        producer.produce(topic, value=message)
+        producer.flush()
+
+    new_message = NewMessageRequest(
+        id='4',
+        author=NewMessageUser(
+            id='5267243',
+            username='Me',
+            fullname='Firstname Secondname',
+        ),
+        chat=NewMessageChat(
+            id='-65347681423',
+            title='Omega Momiji',
+            type=ChatType.GROUP,
+        ),
+        frontend='Telegram 1',
+        type=MessageType.MESSAGE,
+        reply_to='3',
+        text='Hello World!',
+    )
+
+    send_message('frontends.messages.v1', json.dumps(dataclasses.asdict(new_message), indent=2))
+    # access_key = os.environ.get('MINIO_ACCESS_KEY')
+    # secret_key = os.environ.get('MINIO_SECRET_KEY')
+    # client = Minio("localhost:8000",
+    #                secure=False,
+    #                access_key=access_key,
+    #                secret_key=secret_key,
+    #                )
+    # print(
+    #     client.bucket_exists("audio1")
+    # )
+    # with open("/home/kepler-br/Music/Ib/title.mp3", "rb") as fp:
+    #     read = fp.read()
+    #
+    #     client.put_object("audio", "audio_test.mp3", BytesIO(read), len(read))
     pass
 
 if __name__ == '__main__':
-    # main()
-    # sys.exit(0)
+    main()
+    sys.exit(0)
     port: int = int(os.environ.get('SERVER_PORT', '8080'))
     host: str = os.environ.get('SERVER_HOST', '0.0.0.0')
 
